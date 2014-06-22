0ce77b83e5ed42e788b7d9c4c259afe4dc57a747
Omer Rauchwerger <omer@rauchy.net>
2014-06-22 17:30:54 +0300

    Use secret_key_base as key. Also, I've mentioned the different concerns to be implemented in different classes in upcoming commits

--- lib/one_secret.rb
+++ lib/one_secret.rb
@@ -4,9 +4,6 @@ require "encryptor"
 require "one_secret/railtie"
 
 module OneSecret
-  default_options = {:key => "some_key"}
-  Encryptor.default_options.merge!(default_options)
-
   def self.set(environment, key, value)
     secrets = SecretsYAML.new("config/secrets.yml")
     secrets.set(Rails.env, key, value.encrypt)
--- lib/one_secret/railtie.rb
+++ lib/one_secret/railtie.rb
@@ -1,5 +1,21 @@
 module OneSecret
   class Railtie < Rails::Railtie
+    config.after_initialize do
+      # concern 1 - initialize Encryptor with the correct secret_key_base and optional iv and salt
+      default_options = {:key => Rails.application.secrets.secret_key_base}
+      Encryptor.default_options.merge!(default_options)
+
+      # concern 2 - decrypt Rails.application.secrets
+      Rails.application.secrets.each_pair do |key, value|
+        decrypted_value = value.decrypt rescue value
+        Rails.application.secrets[key] = decrypted_value
+      end
+
+      # concern 3 - rake tasks for set / get
+      # concern 4 - security message for people keeping production secret_key_base in secrets.yml
+      # concern 5 - copy all Rails.application.secrets to ENV
+    end
+
     generators do
       require_relative "generators/secrets/secrets_generator"
     end
