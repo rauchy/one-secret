3e20028d6deabe3717411c22685f0031710beae7
Omer Rauchwerger <omer@rauchy.net>
2014-06-22 16:45:24 +0300

    Encrypt values with Encryptor

--- lib/one_secret.rb
+++ lib/one_secret.rb
@@ -1,11 +1,15 @@
 require "one_secret/version"
 require "one_secret/secrets_yaml"
+require "encryptor"
 require "one_secret/railtie"
 
 module OneSecret
+  default_options = {:key => "some_key"}
+  Encryptor.default_options.merge!(default_options)
+
   def self.set(environment, key, value)
     secrets = SecretsYAML.new("config/secrets.yml")
-    secrets.set(Rails.env, key, value)
+    secrets.set(Rails.env, key, value.encrypt)
     secrets.save
   end
 end
