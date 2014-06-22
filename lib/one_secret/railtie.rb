c63ab892170282e397b009604272e4466ae2af5a
Omer Rauchwerger <omer@rauchy.net>
2014-06-22 18:02:51 +0300

    copy all Rails.application.secrets to ENV

--- lib/one_secret/railtie.rb
+++ lib/one_secret/railtie.rb
@@ -1,6 +1,6 @@
 module OneSecret
   class Railtie < Rails::Railtie
-    config.after_initialize do
+    config.before_initialize do
       # concern 1 - initialize Encryptor with the correct secret_key_base and optional iv and salt
       default_options = {:key => Rails.application.secrets.secret_key_base}
       Encryptor.default_options.merge!(default_options)
@@ -13,6 +13,9 @@ module OneSecret
 
       # concern 4 - security message for people keeping production secret_key_base in secrets.yml
       # concern 5 - copy all Rails.application.secrets to ENV
+      Rails.application.secrets.each_pair do |key, value|
+        ENV[key.to_s] = value
+      end
     end
 
     # concern 3 - rake tasks for set / get
