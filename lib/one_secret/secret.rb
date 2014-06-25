09f947d80cb7a9f11478136ecbd17551f985a66e
Omer Rauchwerger <omer@rauchy.net>
2014-06-25 11:58:30 +0300

    OneSecret::Secret supports loading encrypted hashes

--- lib/one_secret/secret.rb
+++ lib/one_secret/secret.rb
@@ -1,7 +1,7 @@
 module OneSecret
   class Secret
     def self.load(encrypted_value)
-      encrypted_value
+      Encryptor.decrypt(encrypted_value) rescue encrypted_value
     end
   end
 end
--- spec/one_secret/secret_spec.rb
+++ spec/one_secret/secret_spec.rb
@@ -2,9 +2,24 @@ require './spec/spec_helper'
 
 module OneSecret
   describe Secret do
+    before do
+      Encryptor.default_options.merge!(key: "f3bbdba9485ac1ee1412d2c839be0a0f")
+    end
+
     describe ".load" do
       it "returns the same value when a non-encrypted hash is provided" do
-        Secret.load("hello").must_equal "hello"
+        Secret.load("I am not encrypted").must_equal "I am not encrypted"
+      end
+
+      it "decrypts encrypted hashes" do
+        encrypted_hash = {
+          value: "\xE6N\x9DF\xDA%\xC2nR\n\x1D\x9F\xE0l\xB1\x1F",
+          iv: "b511f7ffa666467c1c55f466886277b8",
+          salt: "12be65aafdd78c0540146f2f995c95c7",
+          algorithm: "aes-256-cbc"
+        }
+
+        Secret.load(encrypted_hash).must_equal "I am encrypted"
       end
     end
   end
