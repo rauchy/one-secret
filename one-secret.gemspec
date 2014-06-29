0f44ee8ae2743de0f9e1a67d8e7229648672d035
Omer Rauchwerger <omer@rauchy.net>
2014-06-29 17:32:18 +0300

    Use rspec-mocks

--- one-secret.gemspec
+++ one-secret.gemspec
@@ -21,5 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "pry"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec-mocks", "~> 3.0.2"
+  spec.add_dependency "activesupport"
   spec.add_dependency "encryptor"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,25 @@ require './lib/one_secret'
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/pride'
+
+require 'rspec/mocks'
+require 'minitest/autorun'
+require 'rspec/mocks'
+
+module MinitestRSpecMocksIntegration
+  include ::RSpec::Mocks::ExampleMethods
+
+  def before_setup
+    ::RSpec::Mocks.setup
+    super
+  end
+
+  def after_teardown
+    super
+    ::RSpec::Mocks.verify
+  ensure
+    ::RSpec::Mocks.teardown
+  end
+end
+
+Minitest::Spec.send(:include, MinitestRSpecMocksIntegration)
