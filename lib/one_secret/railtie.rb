a001bf832b1910ca5ae7edb1c6d63b86b3581f15
Omer Rauchwerger <omer@rauchy.net>
2014-07-07 11:55:20 +0300

    Use Secret to load values into ENV

--- lib/one_secret/railtie.rb
+++ lib/one_secret/railtie.rb
@@ -15,7 +15,7 @@ module OneSecret
       # concern 4 - security message for people keeping production secret_key_base in secrets.yml
       # concern 5 - copy all Rails.application.secrets to ENV
       Rails.application.secrets.each_pair do |key, value|
-        ENV[key.to_s] = value
+        ENV[key.to_s] = Secret.load(value)
       end
     end
 
