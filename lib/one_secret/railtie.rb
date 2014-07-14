3d9e6b6b070124b2c7ea7d6a4b218072b5d84582
Omer Rauchwerger <omer@rauchy.net>
2014-07-14 21:20:48 +0300

    Iterate over secrets once, decrypt and set them to ENV

--- lib/one_secret/railtie.rb
+++ lib/one_secret/railtie.rb
@@ -3,20 +3,13 @@ module OneSecret
     config.before_initialize do
       Secret.key = KeyResolution.try(:env, :rails, :stdin)
 
-      # concern 2 - decrypt Rails.application.secrets
       Rails.application.secrets.each_pair do |key, value|
-        decrypted_value = Secret.load(value)
-        Rails.application.secrets[key] = decrypted_value
+        Rails.application.secrets[key] = ENV[key.to_s] = Secret.load(value)
       end
 
       # concern 4 - security message for people keeping production secret_key_base in secrets.yml
-      # concern 5 - copy all Rails.application.secrets to ENV
-      Rails.application.secrets.each_pair do |key, value|
-        ENV[key.to_s] = Secret.load(value)
-      end
     end
 
-    # concern 3 - rake tasks for set / get
     rake_tasks do
       load "one_secret/tasks.rake"
     end
