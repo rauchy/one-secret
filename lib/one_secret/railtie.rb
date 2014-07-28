98a9e1058a7768c1cf9499b6c6388c0df3634cb0
Omer Rauchwerger <omer@rauchy.net>
2014-07-28 11:45:31 +0300

    Avoid running on assets:precompile

--- lib/one_secret/railtie.rb
+++ lib/one_secret/railtie.rb
@@ -1,15 +1,27 @@
 module OneSecret
   class Railtie < Rails::Railtie
     config.before_initialize do
-      Secret.key = KeyResolution.try(:env, :rails, :stdin)
+      if should_run?
+        Secret.key = KeyResolution.try(:env, :rails, :stdin)
 
-      Rails.application.secrets.each_pair do |key, value|
-        Rails.application.secrets[key] = ENV[key.to_s] = Secret.load(value)
+        Rails.application.secrets.each_pair do |key, value|
+          Rails.application.secrets[key] = ENV[key.to_s] = Secret.load(value)
+        end
       end
     end
 
     rake_tasks do
       load "one_secret/tasks.rake"
+    end
+
+    private
+
+    def self.should_run?
+      if defined?(Rake)
+        !Rake.application.top_level_tasks.include?("assets:precompile")
+      else
+        true
+      end
     end
   end
 end if defined?(Rails)
